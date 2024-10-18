1a336229ad52abbc815c05880ac280a7746e8ce5
Malcolm Graney <Malcolm.Graney@river-island.com>
2024-10-18 15:29:20 +0800

    Functions Commit 1

---
+++ 05_Functions/README.md
@@ -0,0 +1,20 @@
+
+Q1. A function should have at least one parameter. True or False?
+A1. False there does not need to be any Parameters required in a function.
+
+Q2. In a function, what is the purpose of a return statement?
+A2. The return statment is used to terminate the function and optionally return results of the function to where it was called.
+
+Q3. What is the difference between the result(s) list and the parameter(s) list?
+A3. The Parameter list is the list of parameters that are passed into the function from the calling code, and the results list is the list of results being returned to the calling code as a result of the function being run.
+
+Q4. Can we write Go programs without functions?
+A4. Sure, but this would lead to hard to maintain code if the code was long enoough. Using functions, where suitable, will make the code easier to read and maintain, and allow the re-use of the functions where necessarry, reducing time to change and mistakes if changes to the code are required.  
+We can also use functions written by other devs or std functions available in go, which will save us from rewriting the wheel.
+Also by using std go functions or those written by other devs, we do not need to see the underlying code, so again will keep our code base less cluttered and easier to read.
+
+Q5. Fill in the blank: 
+A5. To launch the execution of a function, I need to call it.
+
+Q6. When I define a function, it will be launched at the beginning of my program. True or False?
+A6. False, we need to call the function for it to be executed (run).
--- 05_Functions/main.go
+++ 05_Functions/main.go
@@ -2,21 +2,124 @@ package main
 
 import "fmt"
 
-func main() {
-	i := add(17, 34)
-	fmt.Println(i)
-}
+/*
+	func main() {
+		i := add(17, 34)
+		fmt.Println(i)
+	}
 
 // return sum of a and y
-func add(x int, y int) int {
-	return x + y
+
+	func add(x int, y int) int {
+		return x + y
+	}
+*/
+/*
+func main() {
+	q, r := myFunction("Hello", "World")
+	fmt.Println(q)
+	fmt.Println(r)
+}
+func myFunction(p1 string, p2 string) (string, string) { //%s is the holder for each string parameter in turn
+	msg1 := fmt.Sprintf("%s function in the %s", p1, p2) // so in this example prints "Hello function in the World"
+	msg2 := fmt.Sprintf("%s %s", p2, p1)                 // and this one prints "World Hello"
+	return msg1, msg2
 }
 
-//func main() {
-//	s, i := myFunction("Hello")
-//	fmt.Println(s, i)
-//}
-//func myFunction(p1 string) (string, int) {
-//	msg := fmt.Sprintf("%s function", p1)
-//	return msg, 10
-//}
+func main() {
+	q, r := myFunction("Hello", "World")
+	fmt.Println(q)
+	fmt.Println(r)
+}
+func myFunction(p1 string, p2 string) (a string, b string) { // Named the returned output strings a & b
+	a = fmt.Sprintf("%s function in the %s", p1, p2) // a and b returned to calling code and stored into q and r respectively
+	b = fmt.Sprintf("%s %s", p2, p1)
+	return
+}
+//
+func main() {
+	myFunction()
+}
+func myFunction() {
+	fn := func() {
+		fmt.Println("inside fn")
+	}
+
+	fn()
+}
+//
+func main() {
+	myFunction()
+}
+
+func myFunction() {
+	func() {
+		fmt.Println("inside anon fn")
+	}()
+}
+//
+func main() { // made fn from above an anonymous function, need the empty parantheses at end to call the function
+	func() {
+		fmt.Println("inside anonymous fn")
+	}()
+}
+*/
+/*
+func main() {
+	johnPrice := computePrice(145.90, 3) //*\label{funcEx1Us1}
+	paulPrice := computePrice(26.32, 10) //*\label{funcEx1Us2}
+	robPrice := computePrice(189.21, 2)  //*\label{funcEx1Us3}
+	total := johnPrice + paulPrice + robPrice
+	fmt.Printf("TOTAL : $%0.2f\n", total) // %0.2f formats the 'total' to 2 decimal places, \n terminates the print line and stops % character from being displayed in Terminal Output
+}
+func computePrice(rate float32, nights int) float32 { //*\label{funcEx1Dec}
+	return rate * float32(nights) //Need to convert int nights to same type as rate, which is float32 for calc to work
+}
+*/
+/*
+func main() {
+	fmt.Println("Before assigning add to myFunction")
+	add := myFunction()
+	fmt.Println("After assigning add to myFunction")
+	add(5)
+	add(2)
+	fmt.Println(add(10))
+
+	fmt.Println("Before addition function call assigned")
+	addition := myFunction()
+	fmt.Println("So as you can see above, the addition function call assigned to myFunction means the variables within are reset")
+	addition(1)
+	addition(1)
+	addition(1)
+}
+func myFunction() func(int) int { // The anonymous function func(v int) is returned
+	sum := 0 //This initialisation section is only performed once when called from an assigned function such as the add() or addition() above
+	fmt.Println(sum)
+	fmt.Println("So this is just the initialisation part which is only performed once. When the function is assigned.")
+	//So v is the parameter passed from the assigned function, in this case from add() and addition().
+	return func(v int) int {
+		sum += v //+= Adds the value on the right to the value in the variable on the left and assigns it to the variable 'sum = sum +v'
+		fmt.Println(v)
+		fmt.Println(sum)
+		fmt.Println()
+		return sum // So this 'return sum' returns the calculation of the anonymous func
+	}
+}
+*/
+//
+func main() {
+	sum := add(1, 2, 3, 5, 8, 13, 21)
+	fmt.Println(sum)
+}
+func add(values ...int) int {
+	sum := 0
+	for i, v := range values {
+		sum += v
+		fmt.Println(i, v)
+		/*
+		   for _, v := range values {  // do not need to use a counter such as i above if we are not going to use it anywhere so can just have
+		   	sum += v                // underscore used
+		*/
+	}
+	return sum
+}
