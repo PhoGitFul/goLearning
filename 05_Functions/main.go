51f1e69870051ff6f0245e4f23a7518d405d1e6b
Malcolm Graney <Malcolm.Graney@river-island.com>
2024-10-21 17:50:13 +0800

    remove comments to gitignore fileh

--- 05_Functions/main.go
+++ 05_Functions/main.go
@@ -2,124 +2,99 @@ package main
 
 import "fmt"
 
-/*
-	func main() {
-		i := add(17, 34)
-		fmt.Println(i)
-	}
-
-// return sum of a and y
-
-	func add(x int, y int) int {
-		return x + y
-	}
-*/
-/*
 func main() {
-	q, r := myFunction("Hello", "World")
+	fmt.Println("Add function to sum two numbers")
+	i := add(17, 34)
+	fmt.Println(i)
+	fmt.Println("\nString function")
+	q, r := hw("Hello", "World")
 	fmt.Println(q)
 	fmt.Println(r)
-}
-func myFunction(p1 string, p2 string) (string, string) { //%s is the holder for each string parameter in turn
-	msg1 := fmt.Sprintf("%s function in the %s", p1, p2) // so in this example prints "Hello function in the World"
-	msg2 := fmt.Sprintf("%s %s", p2, p1)                 // and this one prints "World Hello"
-	return msg1, msg2
-}
 
-func main() {
-	q, r := myFunction("Hello", "World")
-	fmt.Println(q)
-	fmt.Println(r)
-}
-func myFunction(p1 string, p2 string) (a string, b string) { // Named the returned output strings a & b
-	a = fmt.Sprintf("%s function in the %s", p1, p2) // a and b returned to calling code and stored into q and r respectively
-	b = fmt.Sprintf("%s %s", p2, p1)
-	return
-}
-//
-func main() {
-	myFunction()
-}
-func myFunction() {
-	fn := func() {
-		fmt.Println("inside fn")
-	}
+	fmt.Println("\nFunction as a value")
+	funcValue()
 
-	fn()
-}
-//
-func main() {
-	myFunction()
-}
+	fmt.Println("\nAnonymous Function as a value")
+	anonFunction()
 
-func myFunction() {
-	func() {
-		fmt.Println("inside anon fn")
-	}()
-}
-//
-func main() { // made fn from above an anonymous function, need the empty parantheses at end to call the function
-	func() {
-		fmt.Println("inside anonymous fn")
-	}()
-}
-*/
-/*
-func main() {
-	johnPrice := computePrice(145.90, 3) //*\label{funcEx1Us1}
-	paulPrice := computePrice(26.32, 10) //*\label{funcEx1Us2}
-	robPrice := computePrice(189.21, 2)  //*\label{funcEx1Us3}
+	fmt.Println("\nReturn a value from function")
+	johnPrice := computePrice(145.90, 3)
+	paulPrice := computePrice(26.32, 10)
+	robPrice := computePrice(189.21, 2)
 	total := johnPrice + paulPrice + robPrice
-	fmt.Printf("TOTAL : $%0.2f\n", total) // %0.2f formats the 'total' to 2 decimal places, \n terminates the print line and stops % character from being displayed in Terminal Output
-}
-func computePrice(rate float32, nights int) float32 { //*\label{funcEx1Dec}
-	return rate * float32(nights) //Need to convert int nights to same type as rate, which is float32 for calc to work
-}
-*/
-/*
-func main() {
-	fmt.Println("Before assigning add to myFunction")
-	add := myFunction()
-	fmt.Println("After assigning add to myFunction")
+	fmt.Printf("TOTAL : $%0.2f\n", total)
+
+	fmt.Println("\nClosures")
+	fmt.Println("Before assigning 'add' to closureFunc")
+	add := closureFunc()
+	fmt.Println("After assigning 'add' to closureFunc")
 	add(5)
 	add(2)
 	fmt.Println(add(10))
+	fmt.Println("\nEach call to the function uses the same variable within the function hence we can send diff values to the function and get the addition of the values sent")
 
-	fmt.Println("Before addition function call assigned")
-	addition := myFunction()
-	fmt.Println("So as you can see above, the addition function call assigned to myFunction means the variables within are reset")
+	fmt.Println("\nBefore addition function call assigned")
+	addition := closureFunc()
+	fmt.Println("So as you can see above, the addition function call assigned to closureFunc means the variables within are reset")
 	addition(1)
 	addition(1)
 	addition(1)
+	fmt.Println(addition(1))
+
+	fmt.Println("\nVaradic Function")
+	sum := multiAdd(1, 2, 3, 5, 8, 13, 21)
+	fmt.Println(sum)
+
 }
-func myFunction() func(int) int { // The anonymous function func(v int) is returned
-	sum := 0 //This initialisation section is only performed once when called from an assigned function such as the add() or addition() above
+func add(x int, y int) int {
+	fmt.Println(x, "+", y, "=")
+	return x + y
+}
+
+func hw(p1 string, p2 string) (string, string) {
+	msg1 := fmt.Sprintf("%s function in the %s", p1, p2)
+	msg2 := fmt.Sprintf("%s %s", p2, p1)
+	return msg1, msg2
+}
+
+func funcValue() {
+	thisfn := func() {
+		fmt.Println("inside thisfn function")
+	}
+
+	thisfn()
+}
+
+func anonFunction() {
+	func() {
+		fmt.Println("inside anon function")
+	}()
+}
+
+func computePrice(rate float32, nights int) float32 {
+	return rate * float32(nights) //Need to convert int nights to same type as rate, which is float32 for calc to work
+}
+
+func closureFunc() func(int) int {
+	sum := 0
 	fmt.Println(sum)
 	fmt.Println("So this is just the initialisation part which is only performed once. When the function is assigned.")
-	//So v is the parameter passed from the assigned function, in this case from add() and addition().
+
 	return func(v int) int {
-		sum += v //+= Adds the value on the right to the value in the variable on the left and assigns it to the variable 'sum = sum +v'
+		sum += v
 		fmt.Println(v)
 		fmt.Println(sum)
 		fmt.Println()
-		return sum // So this 'return sum' returns the calculation of the anonymous func
+		return sum
 	}
 }
-*/
-//
-func main() {
-	sum := add(1, 2, 3, 5, 8, 13, 21)
-	fmt.Println(sum)
-}
-func add(values ...int) int {
+
+func multiAdd(values ...int) int {
 	sum := 0
 	for i, v := range values {
 		sum += v
 		fmt.Println(i, v)
-		/*
-		   for _, v := range values {  // do not need to use a counter such as i above if we are not going to use it anywhere so can just have
-		   	sum += v                // underscore used
-		*/
+
 	}
 	return sum
 }
